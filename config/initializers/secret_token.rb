380acec30cda2c8cebb4e225827b741aabf4a976
Tatsuya Takamura <takamura@tonchidot.com>
2014-02-09 23:04:46 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyFirstOpsApp::Application.config.secret_token = '778895e979d8f3b345e6dd5582eeaec34e9f6b78c729a91ca4b5c43058adf4d049e75167bef4297da0cbcc2d527034e0499961df139b685d712c70ec0174b8b6'
